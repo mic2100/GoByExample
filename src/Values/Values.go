3b133220609238212e1058489ce0a6b0fe1c2f07
Michael Bardsley <michael.bardsley@symphony-group.co.uk>
2013-12-19 10:00:21 +0000

    Ran go fmt on the values.go file

--- src/Values/Values.go
+++ src/Values/Values.go
@@ -8,10 +8,10 @@ import "fmt"
 func main() {
 	fmt.Println("go" + "lang") //concatenating strings
 
-    fmt.Println("1 + 1 = ", 1 + 1) //integer addition
-    fmt.Println("7.0 / 3.0 = ", 7.0 / 3.0) //float divisions
+	fmt.Println("1 + 1 = ", 1+1)         //integer addition
+	fmt.Println("7.0 / 3.0 = ", 7.0/3.0) //float divisions
 
-    fmt.Println(true && false) //prints boolean false
-    fmt.Println(true || false) //prints boolean true
-    fmt.Println(!true) //prints boolean false
+	fmt.Println(true && false) //prints boolean false
+	fmt.Println(true || false) //prints boolean true
+	fmt.Println(!true)         //prints boolean false
 }
