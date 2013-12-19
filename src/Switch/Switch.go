91f87e3eb5b9469a929600f3526c90f183bd6d62
Michael Bardsley <michael.bardsley@symphony-group.co.uk>
2013-12-19 12:22:08 +0000

    Added the switch examples

---
+++ src/Switch/Switch.go
@@ -0,0 +1,39 @@
+//source: https://gobyexample.com/switch
+//Show an example of using a switch statement
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 2
+	fmt.Print("write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	//multiple expressions can be place in hte same case statement
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("it's the weekend")
+	default:
+		fmt.Println("it's a weekday")
+	}
+
+	//this example shows a switch statement without an expression
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("it's before noon")
+	default:
+		fmt.Println("it's after noon")
+	}
+}
