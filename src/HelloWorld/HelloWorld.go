d743b9e4f7cdb60c147dae141e4f0f1c73e5286c
Michael Bardsley <michael.bardsley@symphony-group.co.uk>
2013-12-19 08:35:17 +0000

    Added HelloWorld example

---
+++ src/HelloWorld/HelloWorld.go
@@ -0,0 +1,10 @@
+//Source: https://gobyexample.com/hello-world
+//A very basic example showing a line of data printed out
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
