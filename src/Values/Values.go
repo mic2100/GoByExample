aadaa2d417524671371b957b196a8ca758884b7b
Michael Bardsley <michael.bardsley@symphony-group.co.uk>
2013-12-19 08:44:39 +0000

    Added Values example

---
+++ src/Values/Values.go
@@ -0,0 +1,17 @@
+//Source: https://gobyexample.com/values
+//A basic example showing how different data types are handled
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang") //concatenating strings
+
+    fmt.Println("1 + 1 = ", 1 + 1) //integer addition
+    fmt.Println("7.0 / 3.0 = ", 7.0 / 3.0) //float divisions
+
+    fmt.Println(true && false) //prints boolean false
+    fmt.Println(true || false) //prints boolean true
+    fmt.Println(!true) //prints boolean false
+}
